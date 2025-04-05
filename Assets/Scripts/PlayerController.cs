c619a3d5f17b8403618d2b56a8b55974f840ced6
Flamer8010 <129384566+Flamer8010@users.noreply.github.com>
2025-04-05 13:39:54 +0200

    temp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -65,6 +65,7 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("DropOff"))
         {
             wood = 0;
+            
            
         }
         else if (other.gameObject.CompareTag("PickUp"))
